b6dcbf245f2fae8641da1c20764f3d895a0616be
anand0202 <102712272+anand0202@users.noreply.github.com>
2022-04-11 18:07:05 +0530

    Getting Json results from Fetch . Not able to store the fetched data inside a global variable.

--- diamond-submission-project/src/App.js
+++ diamond-submission-project/src/App.js
@@ -1,34 +1,25 @@
 import React, { useState, useEffect } from "react";
 
 export default function App() {
-  const CUTWISE_CLIENT_ID =
-    "10_4kcuxoitbkmc4gc4cwkggw04swg0co48ck0soco8w44w8s0c04";
-  const CUTWISE_CLIENT_SECRET =
-    "2zmg3raap64gc8448co0wcg8kwkswswgscwc00ggco880wwk";
-  const CUTWISE_LOGIN = "demo";
-  const CUTWISE_PASSWORD = "UqpY3q3yhLs2xFWt";
-  const [accessToken, setAccessToken] = useState([]);
+  
+  let accessTokenres=[]
 
-  const fetchPost = async () => {
-	  console.log({
-		CUTWISE_CLIENT_ID,
-		CUTWISE_CLIENT_SECRET,
-		CUTWISE_LOGIN,
-		CUTWISE_PASSWORD
-	  })
-    const response = await fetch(
-      `https://cutwise.com/api/oauth/v2/token?grant_type=password&username=${CUTWISE_LOGIN}&password=${CUTWISE_PASSWORD}&client_id=${CUTWISE_CLIENT_ID}&client_secret=${CUTWISE_CLIENT_SECRET}`
-    );
-    const data = await response.json();
-    setAccessToken(data);
-  };
+  fetch("https://cutwise.com/api/oauth/v2/token?grant_type=password&username=demo&password=UqpY3q3yhLs2xFWt&client_id=10_4kcuxoitbkmc4gc4cwkggw04swg0co48ck0soco8w44w8s0c04&client_secret=2zmg3raap64gc8448co0wcg8kwkswswgscwc00ggco880wwk0k")
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .then(accessTokenData=>{
+      accessTokenres.push(accessTokenData)
+    })
+    .catch(error => console.log('error', error));
+
+
 
   useEffect(() => {
-    fetchPost();
+ 
   }, []);
   return (
     <div className="App">
-      <p> {accessToken.value} </p>
+       <p>{accessTokenres}</p>
     </div>
   );
-}
+};
