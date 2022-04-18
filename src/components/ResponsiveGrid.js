f94507ca8f27d1e44d593e3caedd9e761c86387f
RshmanGit <aarsh@alphabi.co>
2022-04-18 09:50:15 +0530

    Removed material UI which fixed the routing Issue

--- src/components/ResponsiveGrid.js
+++ src/components/ResponsiveGrid.js
@@ -1,22 +1,18 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
+
+import '../styles/ResponsiveGrid.scss';
 
 import PerformanceWidget from './performanceWidget';
 
 export default function ResponsiveGrid(props) {
     const { ids } = props;
     return (
-        <Box sx={{ flexGrow: 1 }} style={{ backgroundColor: '#131313' }}>
-            <Grid
-                container
-                spacing={{ xs: 2, sm: 2, md: 2 }}
-                columns={{ xs: 4, sm: 8, md: 12 }}
-            >
+        <div className="grid">
+            <div>
                 {ids.map((id) => (
                     <PerformanceWidget id={id} />
                 ))}
-            </Grid>
-        </Box>
+            </div>
+        </div>
     );
 }
